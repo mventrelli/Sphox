d163d9bdb3b90573491228af0014df6fc882219c
Matt Ventrelli <mventrelli@hawk.iit.edu>
2023-01-20 16:16:43 -0600

    Initial Commit

---
+++ Sphox/SphoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SphoxApp.swift
+//  Sphox
+//
+//  Created by IIT PHYS 440 on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SphoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
